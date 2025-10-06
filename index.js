c8f34b31779998bf2b667233ff62b038a2a28167
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-06 21:23:41 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+const port = process.env.port || 8080;
+
+let clipText = "";
+
+app.get('/', (req, res) => {
+    try{
+        res.status(200).send(clipText);
+        console.log("Read from Clipboard!");
+    }catch(e){
+        res.status(500).send("An error occured while reading text from clipboard.")
+        console.log(e);
+    }
+});
+
+app.post('/', (req, res) => {
+    try{
+        clipText = req.body.text;
+        res.status(200).send("Text Written to Clipboard!");
+    }catch(e){
+        res.status(500).send("An error occured while writting text to clipboard.");
+        console.log(e);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is up and running at ${port}`);
+});
